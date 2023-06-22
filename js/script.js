8e619132857bc9f3d0870f98fbae174d0dcf7f3a
Rahat-Kibria <rahatkibriawahee@gmail.com>
2023-06-23 03:49:11 +0600

    added messenger chat

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ function openProjectTab(event, projectName) {
   event.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+// messenger chat
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "111992678600571");
+chatbox.setAttribute("attribution", "biz_inbox");
+// Your SDK code
+window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: 'v17.0'
+  });
+};
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
